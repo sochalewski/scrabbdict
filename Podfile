b0e94a21f6a520afcbf949e26302c0b4a15ade80
Piotr Sochalewski <sochalewski@gmail.com>
2024-03-26 00:05:01 +0100

    Update third-party dependencies

--- Podfile
+++ Podfile
@@ -1,7 +1,6 @@
 target 'Scrabbdict' do
-  platform :ios, '13.0'
-  
-  use_frameworks!
+  platform :ios, '17.0'
+  use_frameworks! :linkage => :static
 
   pod 'Firebase/Crashlytics'
   pod 'Firebase/Analytics'
@@ -15,20 +14,30 @@ target 'Scrabbdict' do
 end
 
 target 'RealmDatabaseWizard' do
-  platform :macos, '13.0'
-  
-  use_frameworks!
+  platform :macos, '15.0'
+  use_frameworks! :linkage => :static
 
   pod 'RealmSwift'
 end
 
 post_install do |installer|
+  installer.pods_project.build_configurations.each do |config|
+    config.build_settings.delete 'STRIP_INSTALLED_PRODUCT'
+    config.build_settings.delete 'STRIP_STYLE'
+    config.build_settings.delete 'STRIP_SWIFT_SYMBOLS'
+    config.build_settings['DEAD_CODE_STRIPPING'] = 'YES'
+    config.build_settings['LD_NO_PIE'] = 'NO'
+
+    config.build_settings['GCC_C_LANGUAGE_STANDARD'] = 'gnu17'
+    config.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'gnu++20'
+  end
+
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
-      config.build_settings['DEAD_CODE_STRIPPING'] = 'YES'
       config.build_settings.delete 'IPHONEOS_DEPLOYMENT_TARGET'
       config.build_settings.delete 'MACOSX_DEPLOYMENT_TARGET'
       config.build_settings['EXPANDED_CODE_SIGN_IDENTITY'] = ''
+      config.build_settings['CODE_SIGN_IDENTITY'] = ''
       config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
       config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
     end
--- Scrabbdict/Database/RealmObjects.swift
+++ Scrabbdict/Database/RealmObjects.swift
@@ -16,41 +16,33 @@ final class Vocabulary: Object {
         set { _language = newValue.rawValue }
     }
     
-    @objc private dynamic var _language = ""
+    @Persisted private var _language = ""
     
     // List<List<StringObject>> is not allowed :/
-    private let words2 = List<StringObject>()
-    private let words3 = List<StringObject>()
-    private let words4 = List<StringObject>()
-    private let words5 = List<StringObject>()
-    private let words6 = List<StringObject>()
-    private let words7 = List<StringObject>()
-    private let words8 = List<StringObject>()
-    private let words9 = List<StringObject>()
-    private let words10 = List<StringObject>()
-    private let words11 = List<StringObject>()
-    private let words12 = List<StringObject>()
-    private let words13 = List<StringObject>()
-    private let words14 = List<StringObject>()
-    private let words15 = List<StringObject>()
+    @Persisted var words2: List<StringObject>
+    @Persisted var words3: List<StringObject>
+    @Persisted var words4: List<StringObject>
+    @Persisted var words5: List<StringObject>
+    @Persisted var words6: List<StringObject>
+    @Persisted var words7: List<StringObject>
+    @Persisted var words8: List<StringObject>
+    @Persisted var words9: List<StringObject>
+    @Persisted var words10: List<StringObject>
+    @Persisted var words11: List<StringObject>
+    @Persisted var words12: List<StringObject>
+    @Persisted var words13: List<StringObject>
+    @Persisted var words14: List<StringObject>
+    @Persisted var words15: List<StringObject>
     
     /// An array of list of String objects sorted from zero to fifteen letters.
     lazy private(set) var words: [List<StringObject>] = {
         return [List<StringObject>(), List<StringObject>(), words2, words3, words4, words5, words6, words7, words8, words9, words10, words11, words12, words13, words14, words15]
     }()
-    
-    override static func ignoredProperties() -> [String] {
-        return ["words"]
-    }
 }
 
 /// A String object.
 final class StringObject: Object {
     
     /// A String value.
-    @objc dynamic var value = ""
-    
-    override static func indexedProperties() -> [String] {
-        return ["value"]
-    }
+    @Persisted(indexed: true) var value: String
 }
