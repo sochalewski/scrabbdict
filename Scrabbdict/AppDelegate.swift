dc3f6b06468aa8ac645d403226c4df828eeb56b5
Piotr Sochalewski <sochalewski@gmail.com>
2026-04-12 19:09:05 +0200

    Skip Firebase setup in debug builds

--- Scrabbdict/AppDelegate.swift
+++ Scrabbdict/AppDelegate.swift
@@ -15,7 +15,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        #if !targetEnvironment(simulator) && !DEBUG
         FirebaseApp.configure()
+        #endif
 
         return true
     }
